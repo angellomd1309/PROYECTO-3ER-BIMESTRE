3abd6b2e18a59207f8622be28fb260c3d8df0c0b
Angello Gabriel Mansilla Dieguez <127903674+angellomd1309@users.noreply.github.com>
2023-07-14 03:31:16 -0600

    Update MAQUINA_EXPENDEDORA.ino

--- MAQUINA_EXPENDEDORA/MAQUINA_EXPENDEDORA.ino
+++ MAQUINA_EXPENDEDORA/MAQUINA_EXPENDEDORA.ino
@@ -10,7 +10,7 @@ CARNETS: 2022380, 2022449,2022472, 2022323, 2022482,2021430.
 PROYECTO 2, MAQUINA EXPENDEDORA
 */
 
-#include <Wire.h>
+#include <Wire.h> 
 #include <LiquidCrystal_I2C.h> //Incluimos las librerias correspondientes.
 
 const int boton1Pin = 2; //Pines de los botones
@@ -20,7 +20,7 @@ const int boton2Pin = 3;
 const int motor1Pins[] = {4, 5, 6, 7}; //Pines para los motores steper.
 const int motor2Pins[] = {8, 9, 10, 11}; 
 
-const int SIGNALINPUT3 = A0;
+const int SIGNALINPUT3 = A0; //Pines de salida y entrada para la intercomunicacion entre arduinos
 const int SIGNALINPUT4 = A1;
 const int SIGNALOUTPUT = A2;
 
@@ -28,49 +28,62 @@ const int SIGNALOUTPUT = A2;
 const int obstaculoPin = 12; //Detector de monedas(el sensor de obstaculos
 
 const char mensajeInicial[] = "SELECCIONE"; //Hacemos un const char con los mensajes, para ahorrar algo de tiempo.
-const char mensajeInicial2[] = "PRODUCTO"; //Hacemos un const char con los mensajes, para ahorrar algo de tiempo.
+const char mensajeInicial2[] = "PRODUCTO"; 
 const char mensajeMoneda[] = "INSERTE MONEDA";
 const char mensajeProcesando[] = "PROCESANDO...";
 
-int seleccion = 0; //Variable para guardar la selección del usuario
+const char mensajePROD1[] = "S: PRODUCTO1"; //Hacemos unos const char con los mensajes de productos seleccionados.
+const char mensajePROD2[] = "S: PRODUCTO2";
+const char mensajePROD3[] = "S: PRODUCTO3";
+const char mensajePROD4[] = "S: PRODUCTO4";
+
+
 LiquidCrystal_I2C lcd(0x20, 16, 2); // Construimos el I2C
-    int SELECMOTOR;
-      int pos = 0;
+
+
+      int SELECMOTOR; //Variable que guardara la seleccion del usuario.
+      int pos = 0; //Variable tipo contador para los ciclos de girado de los motores stepper.
+
+      
 
 void setup() {
 
-  // Configurar los pines de los botones como entradas
-  pinMode(boton1Pin, INPUT_PULLUP);
+  pinMode(boton1Pin, INPUT_PULLUP);   // Configurar los pines de los botones como entradas
   pinMode(boton2Pin, INPUT_PULLUP);
 
-  // Configurar los pines de los motores stepper como salidas
-  for (int i = 0; i < 4; i++) {
+
+  for (int i = 0; i < 4; i++) {   // Configurar los pines de los motores stepper como salidas
     pinMode(motor1Pins[i], OUTPUT);
     pinMode(motor2Pins[i], OUTPUT);
   }
 
-  // Configurar el pin del detector de obstáculos como entrada
-  pinMode(obstaculoPin, INPUT);
+
+  pinMode(obstaculoPin, INPUT);   // Configurar el pin del detector de obstáculos como entrada
   pinMode(SIGNALINPUT3, INPUT);
   pinMode(SIGNALINPUT4, INPUT);
   pinMode(SIGNALOUTPUT, OUTPUT);  
   
 
-  // Inicializar el LCD
-  lcd.init();
+
+  lcd.init();   // Inicializar el LCD
   lcd.backlight();
   lcd.setCursor(0,0);
-  lcd.print(mensajeInicial);
+  lcd.print(mensajeInicial); //Mostrar los mensajes iniciales.
   delay(100);
   lcd.setCursor(0,1);
   lcd.print(mensajeInicial2);
 }
 
 void loop() {
-  if (digitalRead(boton1Pin) == LOW) {
+  
+  if (digitalRead(boton1Pin) == LOW) { //PRODUCTO UNO SELECCIONADO.
     SELECMOTOR = 1;
     lcd.clear();
-    lcd.print(mensajeMoneda);}
+    lcd.print(mensajeMoneda);
+    delay(100);
+    lcd.setCursor(0,1);
+    lcd.print(mensajePROD1);  
+    }
 
   if (digitalRead(obstaculoPin) == LOW) {
     lcd.clear();
@@ -79,14 +92,20 @@ void loop() {
     ejecutarMotor();
     SELECMOTOR = 0;
     reiniciar();
-    }  
+    }  // FIN DEL PRODUCTO 1.
     
-  // Verificar si se ha presionado algún botón
-    if (digitalRead(boton2Pin) == LOW) {
+
+
+
+    if (digitalRead(boton2Pin) == LOW) { //PRODUCTO DOS SELECCIONADO.
     SELECMOTOR = 2;
     lcd.clear();
-    lcd.print(mensajeMoneda);}
-    //Si hay deteccion de la moneda, el codigo proseguira.
+    lcd.print(mensajeMoneda);
+    delay(100);
+    lcd.setCursor(0,1);
+    lcd.print(mensajePROD2);       
+    }
+    
   if (digitalRead(obstaculoPin) == LOW) {
     lcd.clear();
     lcd.setCursor(0,0);
@@ -94,12 +113,20 @@ void loop() {
     ejecutarMotor();
     SELECMOTOR = 0;    
     reiniciar();
-    } 
+    }  // FIN DEL PRODUCTO 2.
 
-  if (digitalRead(SIGNALINPUT3) == HIGH) {
+
+
+    
+
+  if (digitalRead(SIGNALINPUT3) == HIGH) { //PRODUCTO TRES SELECCIONADO.
     SELECMOTOR = 3;
     lcd.clear();
-    lcd.print(mensajeMoneda);}
+    lcd.print(mensajeMoneda);
+    delay(100);
+    lcd.setCursor(0,1);
+    lcd.print(mensajePROD3);
+    }
 
   if (digitalRead(obstaculoPin) == LOW) {
     digitalWrite(SIGNALOUTPUT, HIGH);
@@ -110,12 +137,21 @@ void loop() {
     digitalWrite(SIGNALOUTPUT, LOW);
     SELECMOTOR = 0;    
     reiniciar();
-    }  
+    }  // FIN DEL PRODUCTO 3.
 
-  if (digitalRead(SIGNALINPUT4) == HIGH) {
+
+
+
+
+  if (digitalRead(SIGNALINPUT4) == HIGH) { //PRODUCTO CUATRO SELECCIONADO.
     SELECMOTOR = 4;
     lcd.clear();
-    lcd.print(mensajeMoneda);}
+    lcd.print(mensajeMoneda);
+    delay(100);
+    lcd.setCursor(0,1);
+    lcd.print(mensajePROD4);       
+    }
+
 
   if (digitalRead(obstaculoPin) == LOW) {
     digitalWrite(SIGNALOUTPUT, HIGH);
@@ -126,20 +162,17 @@ void loop() {
     digitalWrite(SIGNALOUTPUT, LOW);    
     SELECMOTOR = 0;    
     reiniciar();
-    }  
+    }  // FIN DEL PRODUCTO 4.
     
 }
     
     
   
 
-void ejecutarMotor(void) {
-
-
+void ejecutarMotor(void) { //Funcion para realizar los movimientos Stepper.
 
   
-  
-  int demora = 100;
+  int demora = 100; //Variable para provocar demora.
 
 
   if (SELECMOTOR == 1) {  
@@ -293,5 +326,19 @@ void reiniciar() { //Funcion para reiniciar
   delay(100);
   lcd.setCursor(0,1);
   lcd.print(mensajeInicial2);
-
 }
+
+/* ATENCION, EL CODIGO SEGUIRA
+ * EN EL SIGUIENTE CODIGO, EL SIGUIENTE 
+ * CONTROLA LOS BOTONES 3 Y 4,
+ * INCLUYENDO LOS MOTORES 3 Y 4, SI SE DESEARA
+ * IMPLEMENTAR ESTE CODIGO EN UN ARDUINO, SE RECOMIENDA
+ * USAR UN ARDUINO MEGA O GIGA EN SU DEFECTO, YA QUE
+ * ESTOS POSEEN UNA CANTIDAD DE PINES EXTREMA,
+ * O USAR UN EXPANSOR DE PINES, NO LO USAMOS AQUI
+ * YA QUE SOLO SE NOS LIMITABA A EXPANSORES I2C, Y SE
+ * DESCARTARON POR SU DIFICULTAD DE USO. 
+ * 
+ * CODIGO POR: Angello Gabriel Mansilla Dieguez.
+ */
+
